49db8de8ade0459e9e2292fcdaabf7b34700cc0b
j3dev <j3pydev@gmail.com>
2020-04-11 13:47:31 -0700

    first commit

---
+++ client-throttle.js
@@ -0,0 +1,212 @@
+ /* 
+ * Note: I will use the following format for the JSON object returned from the server (on success):
+ * { url: <string>, exists: <true|false>, file: <true|false>, folder: <true|false>, error: <string> }
+ */
+
+ window.onload = () => {
+  /* 
+  * Validate URL per the URL spec (https://owasp.org/www-community/OWASP_Validation_Regex_Repository)
+  * Note: this can be simplified if we only want to handle a subset of valid URLs, such as by using
+  * this regex for domain names (a non-vetted suggestion from OWASP): ^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$
+  * or simply checking the URL path and dropping the rest of the string (e.g., to check URL paths only on the current domain)
+  */
+  const urlRegex = new RegExp("^((((https?|ftps?|gopher|telnet|nntp):\/\/)|(mailto:|news:))(%[0-9A-Fa-f]{2}|[-()_.!~*';/?:@&=+$,A-Za-z0-9])+)([).!';/?:,][[:blank:|:blank:]])?$");
+  // our api url
+  const apiUrl = "example.com/checkUrl";  // our api url
+  const timerCheck = 60 * 1000;  // # of milliseconds to wait for each check of localStorage (1 minute)
+  const reqLimit = 10;  // # of allowed requests before throttling
+  const throttlePeriod = 30 * 60 * 1000;  // # of milliseconds to wait (30 minutes)
+  const infoText = "The number of searches is limited to 10 every 30 minutes.";  // information text for the user
+
+  // request counter
+  let reqCount = 0;
+  // our html elements
+  let infoElem = document.getElementById('info');
+  let urlInput = document.getElementById('urltextbox');
+  let urlInputErr = document.getElementById('urlerror');
+  let urlResultsTitle = document.getElementById('urlResultsTitle');
+  let urlResults = document.getElementById('urlResults');
+
+  // add informational text
+  infoElem.textContent = infoText;
+
+  // update error message on UI
+  let updateUrlError = (msg) => {
+    urlInputErr.textContent = msg;
+  }
+
+  // update url results on UI
+  let updateUrlResults = (jresp) => {
+    let resultsText = jresp.error ? 
+      `API Error: ${jresp.error} ... URL: ${jresp.url}` :
+      `Exists: ${jresp.exists},  File: ${jresp.file},  Folder: ${jresp.folder},  URL: ${jresp.url}`;
+    urlResults.textContent = resultsText;
+    urlResultsTitle.hidden = false;
+    urlResults.hidden = false;
+  }
+
+  // validate the URL
+  let validateURL = (url) => {
+    // check if URL passes regex
+    if (!urlRegex.test(url)) {
+      updateUrlError("Error: URL is invalid.");
+      return false;
+    }
+    // check length of URL string before anything else (len <= 2000)
+    if (url.length > 2000) {
+      updateUrlError("Error: URL is too long.");
+      return false;
+    }
+    // clear error message if no error
+    updateUrlError("");
+    return true;
+  }
+
+  // use fetch api to make request
+  let fetchReq = (jbody) => {
+    async function postData(apiUrl = '', data = {}) {
+      // Default options are marked with *
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        mode: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        referrerPolicy: 'no-referrer',
+        body: data
+      });
+      return await response;
+    }
+
+    postData(apiUrl, jbody)
+      .then((resp) => {
+        if (!resp.ok) {
+          updateUrlError(`Error: ${resp.statusText}`)
+        }
+        return response.json()
+      })
+      .then((jresp) => {
+        updateUrlResults(jresp);
+      })
+      .catch((error) => {
+        updateUrlError('Error:', error);
+      });
+  };
+
+  // use XMLHttpRequest and ES5 if you need to fallback for older browsers
+  var oldReq = function(jbody) {
+    // create XML HTTP request
+    var req = new XMLHttpRequest();
+    req.responseType = 'json';
+
+    // handle various non-http errors
+    req.onerror = function () {
+      updateUrlError('Error: ' + req.status);
+    };
+    req.ontimeout = function () {
+      updateUrlError('Timeout: ' + req.status);
+    };
+    req.onabort = function () {
+      updateUrlError('Aborted: ' + req.status);
+    };
+
+    // onload assumes req.responseText is json
+    req.onload = function () {
+      if (req.status != 200) {
+        // handle http error
+        return updateUrlError('HTTP Error: ' + req.status);
+      }
+      let jresp = JSON.parse(req.response);
+      updateUrlResults(jresp);
+    };
+
+    // asynchronous is true
+    req.open('POST', apiUrl, true);
+    // send the request
+    req.send(jbody);
+  };
+
+  // check localStorage to see if throttle period is finished
+  let throttleTimer = () => {
+    let throttleExpiration = localStorage.getItem('throttleExpiration');
+    // if throttleExpiration was removed from local storage this will still work
+    if (throttleExpiration < Date.now()) {
+      // reset all the throttle things
+      localStorage.removeItem('throttleExpiration');
+      reqCount = 0;
+      urlInput.disabled = false;
+      updateUrlError("");
+      clearInterval(throttleTimer);
+      // re-add event listener for changes in user input
+      urlInput.addEventListener("keyup", urlInputAction);
+    }
+  };
+
+  // handle throttle
+  let throttleHandler = (throttleExpiration, url) => {
+    // remove the unnecessary listener (will add it back later)
+    urlInput.removeEventListener('keyup', urlInputAction);
+    // create date object
+    let expiration = new Date(parseInt(throttleExpiration));
+    updateUrlError("You've reached the maximum number of searches allowed.");
+    updateUrlResults({ 
+      url: url, 
+      exists: false, 
+      file: false, 
+      folder: false, 
+      error: `Wait period ends at ${expiration.toTimeString()} on ${expiration.toDateString()}.`
+    });
+    // set interval to check the throttle expiration once per minute
+    setInterval(throttleTimer, timerCheck);
+  };
+
+  let urlInputAction = (e) => {
+    let url = e.target.value;
+    if (validateURL(url)) {
+      // get throttle expiration date from local storage
+      let throttleExpiration = localStorage.getItem('throttleExpiration');
+      // increase request count
+      reqCount++;
+      // format url into json for server
+      let jbody = JSON.stringify({
+        url: url
+      });
+
+      // test with mock response
+      // check for request count
+      if ((reqCount > reqLimit) && !throttleExpiration) {
+        throttleExpiration = Date.now() + throttlePeriod;
+        localStorage.setItem('throttleExpiration', throttleExpiration);
+        throttleHandler(throttleExpiration, url);
+        urlInput.disabled = true;
+
+      // check for existing throttle expiration
+      } else if (throttleExpiration && (throttleExpiration > Date.now())) {
+        throttleHandler(throttleExpiration, url);
+        urlInput.disabled = true;
+
+      // otherwise, make the mock request
+      } else {
+        if (window.fetch) {
+          updateUrlResults({ url: url, exists: true, file: true, folder: false, error: ""});
+        } else {
+          oldReq(jbody);
+        }
+      }
+
+      // otherwise make post request to server
+      // } else {
+      //   if (window.fetch) {
+      //     fetchReq(jbody);
+      //   } else {
+      //     oldReq(jbody);
+      //   }
+      // }
+      
+    }
+  };
+
+  // event listener for changes in user input
+  urlInput.addEventListener("keyup", urlInputAction);
+
+};
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Client-side Throttling with JavaScript
+
+## This is a weird example to try/test client-side throttling
+
+## It is a good exercise in contemplating throttling as well as user experience
+
+## This is roughly how I define throttling: 
+Once the limit of the number of requests, X, is reached, limit the number of requests to Y, during time period Z.
+
+## Notes about the code in practice:
+ - In the code (`client-throttle.js`) I have X = 10 requests, Y = 0 requests, Z = 30 minutes.  These values are more like what you see for password inputs where you want to limit the attempts over time so that a password can't be brute forced.
+ - There are many other possible combinations of X, Y and Z that could be used in combination with user feedback such as error messages, informational text about expected usage, only making requests once keystrokes have stopped for a set time period, etc.
+ - I have added the X and Z values as constants (`reqLimit` and `throttlePeriod`, respectively) so that they may be easily changed.  I use `localStorage` to track the value for Z so that it can be arbitrarily long and won't change if a user refreshes the page.  Although, of course, if the user deletes their browser's data for this page then it will be reset.  Another caveat is that a user may not allow this site to use `localStorage`.  We would need a fallback for that case, such as holding the value in memory, but then refreshing the page will reset the value. 
+ - The bullet point above highlights one of the obvious issues with throttling on the client-side: there is no way to prevent someone from bypassing this kind of throttling.  To truly limit requests to your API endpoint you must implement server-side throttling.
+ - No matter which combination of X, Y, and Z we use we need to consider the user experience and how we want to communicate this information so that the user understands what is expected of them.  I provide informational text to the user both before and after throttling as well as block the input after throttling starts.  
+ - Suggestions on how to make this webpage better:
+   - Don't make a request on every keystroke if you are concerned about the number of requests being sent, otherwise the user will likely hit they limit of requests as they are typing.  That is not a good user experience.
+   - Dynamically display the number of requests left.
+   - Suggest to the user to copy and paste the URL they want to check, otherwise they will likely hit they limit of requests as they are typing.  That is not a good user experience.
+   - Dynamically display the number of requests left.
+   - Dynamically display the amount of time left once throttling begins.
+   - Right now, the code does not begin the timer until the request limit has been reached.  Depending on the use case, a more logical way to implement this might be to check for the number of requests over a rolling time period.  That way you only need the values of Y and Z.  For example, only allow up to 10 requests in any 30 minute time period.
+
+## Running the code:
+To test it, I'd recommend changing the `throttlePeriod` constant to 1 minute.  Also, please note that using `localStorage` in a broswer requires a valid domain name so you will need to serve the static files, for example, on `localhost`.  Here is a list of one-line servers: https://gist.github.com/willurd/5720255  I used this Golang one because it was convenient for me: https://github.com/kidoman/serve 
